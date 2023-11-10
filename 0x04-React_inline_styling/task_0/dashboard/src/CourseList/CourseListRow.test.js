d5129771ea51c38f3de9039256e94a3f9a4d166b
Arden007 <cloveryuno664@gmail.com>
2023-11-10 13:18:46 +0200

    fix

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -12,13 +12,13 @@ describe('Test CourseListRow component', () => {
     it("should render 2 cell when both text cells are defined", () => {
         const list = shallow(<CourseListRow isHeader={true} textFirstCell="test 1" textSecondCell="test 2" />)
         expect(list.find('th')).toHaveLength(2)
-        expect(list.find("tr").at(0).text()).toEqual('test 1');
-        expect(list.find("tr").at(1).text()).toEqual('test 2');
+        expect(list.find("th").at(0).text()).toEqual('test 1');
+        expect(list.find("th").at(1).text()).toEqual('test 2');
     })
     it('should render 2 td element when isHead = false', () => {
         const list = shallow(<CourseListRow isHeader={true} textFirstCell="test 1" textSecondCell="test 2" />)
         expect(list.find('td')).toHaveLength(2)
-        expect(list.find("tr").at(0).text()).toEqual("test 1");
-        expect(list.find("tr").at(1).text()).toEqual("test 2 ");
+        expect(list.find("td").at(0).text()).toEqual("test 1");
+        expect(list.find("td").at(1).text()).toEqual("test 2 ");
     })
 })
