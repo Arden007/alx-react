d8a77ba6869475f27fb5a4c6f4d393e425dba355
Arden007 <cloveryuno664@gmail.com>
2023-11-13 20:59:10 +0200

    0x05

--- 0x04-React_inline_styling/task_4/dashboard/src/App/App.js
+++ 0x04-React_inline_styling/task_4/dashboard/src/App/App.js
@@ -62,7 +62,18 @@ class App extends React.Component {
             </BodySectionWithMarginBottom>
           )}
           <BodySection title="News from the School">
-            <p>Random Text</p>
+            <p>
+              What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently
+              with desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </p>
           </BodySection>
           <div className={css(footerStyles.footer)}>
             <Footer />
@@ -86,7 +97,7 @@ const footerStyles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    borderTop: "3px solid #E11D3F",
+    borderTop: "3px solid red",
     padding: "1rem",
     fontStyle: "italic",
   },
